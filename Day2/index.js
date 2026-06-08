e0bf9bba093e2a20ae3f826dac1f4fe49ab296bd
sunilgyawali94@gmail.com <sunilgyawali94@gmail.com>
2026-06-08 20:08:56 +0545

    JSON as a server response

--- Day2/index.js
+++ Day2/index.js
@@ -1,5 +1,6 @@
 // Creating the server using the http module
 const http = require("http"); // step 1 acquire http--> import this http modeule in program
+const { json } = require("stream/consumers");
 
 //  const server= http.Server((req, res)=>{ //step2 :"http modeule"  call server .. by arrow callback function
 //     res.end("This is the require data") // step3 : this response is shown in browser and provide to the user
@@ -8,16 +9,28 @@ const http = require("http"); // step 1 acquire http--> import this http modeule
 // we can give the response as html,json(API,REact) ect
 
 // lets provide the html tags as a response
-const server = http.Server((req, res) => {
-  res.end(`
-        <h1>HTML as a response  </h1>
-        <p>This is an Example that, We can send the html as a response</p>
-      `);
-});
+// const server = http.Server((req, res) => {
+//   res.end(`
+//         <h1>HTML as a response  </h1>
+//         <p>This is an Example that, We can send the html as a response</p>
+//       `);
+// });
+
+// .........................................................................
+// providing the json as a response 
+
+const server= http.Server((req, res)=>{
+    res.end(
+        JSON.stringify({
+            name: "Sunil Gyawali",
+            learn :"AI powered MERN-STACK"
+        }
+
+        )
+    )
+})
 
 
-
-
-server.listen(8001, () => {
-  console.log("the server is running  port=8000 ");
+server.listen(8002, () => {
+  console.log("the server is running  port=8002 ");
 });
