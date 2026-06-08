98e5f13decc5dc30061ec2c7181193668471b332
sunilgyawali94@gmail.com <sunilgyawali94@gmail.com>
2026-06-08 20:00:07 +0545

    Providing the HTML as a server response

--- Day2/index.js
+++ Day2/index.js
@@ -1,11 +1,23 @@
 // Creating the server using the http module
-const http=require('http') // step 1 acquire http--> import this http modeule in program
-const { Server } = require('tls')
+const http = require("http"); // step 1 acquire http--> import this http modeule in program
 
- const server= http.Server((req, res)=>{ //step2 :"http modeule"  call server .. by arrow callback function 
-    res.end("This is the require data") // step3 : this response is shown in browser and provide to the user 
-})
+//  const server= http.Server((req, res)=>{ //step2 :"http modeule"  call server .. by arrow callback function
+//     res.end("This is the require data") // step3 : this response is shown in browser and provide to the user
+// })
 
-server.listen(8000, () =>{
-    console.log("the server is running  port=8000 ")
-})
+// we can give the response as html,json(API,REact) ect
+
+// lets provide the html tags as a response
+const server = http.Server((req, res) => {
+  res.end(`
+        <h1>HTML as a response  </h1>
+        <p>This is an Example that, We can send the html as a response</p>
+      `);
+});
+
+
+
+
+server.listen(8001, () => {
+  console.log("the server is running  port=8000 ");
+});
