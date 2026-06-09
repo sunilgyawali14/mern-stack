56f93dfd5d163ddd7eb7f1ffeaf3b5c2ebc84e76
sunilgyawali94@gmail.com <sunilgyawali94@gmail.com>
2026-06-09 07:57:31 +0545

    This is the asynchronous way of reading file

--- Day3/file.js
+++ Day3/file.js
@@ -1,13 +1,18 @@
 // creating file from server using the built in module fs
 const fs=require('fs')
 
-// creating a file by using fs => Asynchronous way
-fs.writeFile('Readme.txt','This is the file created by server', (err)=>{
+// // creating a file by using fs => Asynchronous way
+// fs.writeFile('Readme.txt','This is the file created by server', (err)=>{
+//     if(err) throw err
+//     console.log('The file is created sucessfully "Asynchronous" ');
+// }) 
+
+// // creating a file using fs -> synchronous way 
+// fs.writeFileSync('syncronous.txt','syncronous way of creating file')
+// console.log('successfully crated a synchronous file');
+
+// Asynchronous way of reading file ;
+fs.readFile('Readme.txt','utf-8',(err, data)=>{
     if(err) throw err
-    console.log('The file is created sucessfully "Asynchronous" ');
-}) 
-
-// creating a file using fs -> synchronous way 
-fs.writeFileSync('syncronous.txt','syncronous way of creating file')
-console.log('successfully crated a synchronous file');
-
+    console.log(data);
+})
